5dc4b31f01b84859a7c510067f38c7934f020f4b
Blend3r <141166397+Blend3r@users.noreply.github.com>
2025-03-04 08:07:07 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Ideative
+// made by somebody
+
+const title = "Ideative";
+const subtitle = "made by somebody";
+const content = "nothing here yet";
+
+console.log(`# ${title}\n\n### ${subtitle}\n\n${content}`);
